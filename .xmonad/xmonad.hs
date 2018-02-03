96463e7a629e141a93de9cb544b44f72a9315694
tarnacious <tarn@tarnbarford.net>
2018-02-03 14:33:28 +0100

    Update xmonad config

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,4 +1,17 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
+import XMonad.Util.EZConfig (additionalKeys)
+import XMonad.Util.Run(spawnPipe)
 
-main = xmonad =<< xmobar defaultConfig { modMask = mod4Mask }
+main = xmonad =<< xmobar defaults
+
+myModMask = mod1Mask
+
+defaults = defaultConfig {
+	modMask = myModMask
+	, terminal = "gnome-terminal"
+} `additionalKeys`
+	[
+		((myModMask, xK_p), spawn "dmenu_run -fn 'Terminus' | dmenu -b")
+	]
+
